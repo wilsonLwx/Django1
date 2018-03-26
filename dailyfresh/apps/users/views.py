cb0014737ee72625451cd949b5fbb3dba6697cfc
wilsonLWX <15000108650@163.com>
2018-03-26 21:53:21 +0800

    register

--- dailyfresh/apps/users/views.py
+++ dailyfresh/apps/users/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import View
+
+
+# 注册的类视图
+class RegisterView(View):
+    def get(self, request):
+        return render(request, 'register.html')
+
+    def post(self, request):
+        return HttpResponse('ok')
--- dailyfresh/dailyfresh/settings.py
+++ dailyfresh/dailyfresh/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'dailyfresh.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -113,3 +113,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 指定静态文件存放目录
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
