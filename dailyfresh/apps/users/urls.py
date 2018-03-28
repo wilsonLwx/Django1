5552b3bfcf53b4f8785d3cd24491ba93ea14c696
wilsonLWX <15000108650@163.com>
2018-03-28 11:35:30 +0800

    url

--- dailyfresh/apps/users/urls.py
+++ dailyfresh/apps/users/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from users import views
 
 urlpatterns = [
-    url(r'^register$', views.RegisterView.as_view(), name='register')
+    url(r'^register$', views.RegisterView.as_view(), name='register'),
 ]
