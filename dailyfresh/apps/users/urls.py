9310163ae81073e8bb0f7067509c9228b7bfa4e3
wilsonLWX <15000108650@163.com>
2018-03-28 15:12:24 +0800

    获取用户输入 校验邮箱

--- dailyfresh/apps/users/urls.py
+++ dailyfresh/apps/users/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import url
-
 from users import views
 
 urlpatterns = [
--- dailyfresh/apps/users/views.py
+++ dailyfresh/apps/users/views.py
@@ -1,5 +1,8 @@
+import re
+
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.views.generic import View
@@ -11,4 +14,16 @@ class RegisterView(View):
         return render(request, 'register.html')
 
     def post(self, request):
+        # 获取用户信息
+        username = request.POST.get('user_name')
+        password = request.POST.get('pwd')
+        email = request.POST.get('email')
+        allow = request.POST.get('allow')
+        # 校验信息
+        if not all([username,password,email,allow]):
+            return redirect(reverse('users:register'))
+
+        if not re.match(r'^[a-z0-9][\w\.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$',email):
+            return render(request,'register.html',{'errmsg':'邮箱错误'})
+
         return HttpResponse('ok')
