2e4a4b0cc4ee0937957af01f314fe6737fd44177
wilsonLWX <15000108650@163.com>
2018-04-14 17:20:30 +0800

    商品详情页

--- dailyfresh/apps/goods/urls.py
+++ dailyfresh/apps/goods/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from goods import views
 
 urlpatterns = [
-    url(r'^index$', views.IndexView.as_view(),name='index'),
+    url(r'^index$', views.IndexView.as_view(), name='index'),  # 商品主页
+    url(r'^detail/(?P<sku_id>\d+)$', views.DetailView.as_view(), name='detail'),  # 商品详情页
 ]
--- dailyfresh/apps/goods/views.py
+++ dailyfresh/apps/goods/views.py
@@ -1,5 +1,6 @@
 from django.core.cache import cache
-from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.template import loader
@@ -7,7 +8,7 @@ from django.views.generic import View
 from django_redis import get_redis_connection
 
 from goods.models import GoodsCategory, IndexGoodsBanner, \
-    IndexPromotionBanner, IndexCategoryGoodsBanner
+    IndexPromotionBanner, IndexCategoryGoodsBanner, GoodsSKU
 
 
 class IndexView(View):
@@ -60,6 +61,8 @@ class IndexView(View):
 
         cart_num = 0
         user = request.user
+
+        # 用户已经登录
         if user.is_authenticated():
             # 获取redis链接实例
             redis_conn = get_redis_connection('default')
@@ -73,3 +76,71 @@ class IndexView(View):
         context['cart_num'] = cart_num
 
         return render(request, 'index.html', context)
+
+
+class DetailView(View):
+    """商品详细信息页面"""
+
+    def get(self, request, sku_id):
+        # 尝试获取缓存数据
+        context = cache.get("detail_%s" % sku_id)
+        # 如果缓存没数据 就去数据库中查询数据
+        if context is None:
+            try:
+                # 获取商品信息
+                sku = GoodsSKU.objects.get(id=sku_id)
+            except GoodsSKU.DoesNotExist:
+                return redirect(reverse('goods:index'))
+
+            # 获取类别
+            categorys = GoodsCategory.objects.all()
+
+            # 从订单中获取评论信息
+            sku_orders = sku.ordergoods_set.all().order_by('-create_time')[:30]
+            if sku_orders:
+                for sku_order in sku_orders:
+                    sku_order.ctime = sku_order.create_time.strftime('%Y-%m-%d %H:%M:%S')
+                    sku_order.username = sku_order.order.user.username
+            else:
+                sku_order = []
+
+            # 获取最新推荐
+            new_skus = GoodsSKU.objects.filter(category=sku.category).order_by('-create_time')[:2]
+
+            # 获取其他规格的商品
+            other_skus = sku.goods.goodssku_set.exclude(id=sku_id)
+
+            context = {
+                'categorys': categorys,
+                'sku': sku,
+                'orders': sku_orders,
+                'new_skus': new_skus,
+                'other_skus': other_skus
+            }
+            # 设置缓存
+            cache.set("detail_%s" % sku_id, context, 3600)
+
+        cart_num = 0
+        user = request.user
+
+        # 用户已经登录
+        if user.is_authenticated():
+            # 获取redis链接实例
+            redis_conn = get_redis_connection('default')
+
+            # 获取购物车数据
+            cart_dict = redis_conn.hgetall('cart_%s' % user.id)
+            for cart in cart_dict.values():
+                # 计算购物车的总数量
+                cart_num += int(cart)
+            # 移除已经存在的本商品浏览历史记录
+            redis_conn.lrem('history_%s' % user.id, 0, sku_id)
+            # 添加新的浏览记录
+            redis_conn.lpush('history_%s' % user.id, sku_id)
+            # 只保存最多5条记录
+            redis_conn.ltrim('history_%s' % user.id, 0, 4)
+
+        # 将数据添加到context里
+        context['cart_num'] = cart_num
+
+        return render(request, 'detail.html', context)
